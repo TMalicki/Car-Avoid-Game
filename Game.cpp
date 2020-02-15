8dea634a957b297a197dfb27bfa1684ef8195899
Tomasz Malicki <t.malicki1@wp.pl>
2020-02-15 20:19:51 +0100

    gameOver screen tests
    Testing game over screen with option to play again or not

--- Game.cpp
+++ Game.cpp
@@ -23,6 +23,18 @@ void Game::setup()
 	{
 		road[i].setStripPosition(&windowSize.x);
 	}
+
+	tEndScreen.loadFromFile("sprites/gameover.png");
+	tChooseBar.loadFromFile("sprites/chooseBar.png");
+
+	sEndScreen.setTexture(tEndScreen);
+	sEndScreen.setScale(0.7, 0.7);
+	sEndScreen.setPosition(25, 100);
+
+	sChooseBar.setTexture(tChooseBar);
+	sChooseBar.setColor(sf::Color(255, 255, 255, 128));
+	sChooseBar.setScale(0.7, 0.7);
+	sChooseBar.setPosition(178, 462);
 }
 
 bool Game::run()
@@ -31,13 +43,15 @@ bool Game::run()
 	{
 		updateEvents(window, event);
 
+		if (theEnd == false)
+		{
 		dt = clock.restart().asSeconds();
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) car->setDir(sf::Vector2i(1, 0));
 		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) car->setDir(sf::Vector2i(-1, 0));
 		else car->setDir(sf::Vector2i(0, 0));
 
-		if (Bullet::Recoil()) 
+		if (Bullet::Recoil())
 		{
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 			{
@@ -45,17 +59,17 @@ bool Game::run()
 			}
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::R) && car->getReloading() == false) car->setReloading(true);
-					
-		if(car->getReloading() == true) car->reload();
 
-		if (spawnTime > 2.0) 
+		if (car->getReloading() == true) car->reload(dt);
+
+		if (spawnTime > 2.0)
 		{
 			traffic.push_back(new Traffic{ 0.0, 9.0f + 1.0f * Player::getLvl() });
 			traffic.back()->saveSpriteSettings(windowSize);
 			spawnTime = 0.0;
 		}
 
-		spawnTime += dt + (0.005 * Player::getLvl());
+		spawnTime += dt + (0.0005 * Player::getLvl());
 
 		if (traffic.size() != 0)
 			for (int i = 0; i < traffic.size(); i++)
@@ -72,9 +86,13 @@ bool Game::run()
 
 		car->setText();
 
-		for(int i = 0; i < road.size();i++)
+		for (int i = 0; i < road.size(); i++)
 			road[i].move(windowSize, dt, Player::getLvl());
+		}
+		else if (theEnd == true)
+		{
 
+		}
 		draw();
 	}
 	return true;
@@ -97,9 +115,7 @@ void Game::draw()
 			window->draw(traffic[i]->getSprite());
 	/*  -------------------------------------------------  */
 	window->draw(car->getSprite());
-	//window->draw(car->getBounds());
 	window->draw(car->getCollisionArea());
-	//window->draw(car->getSprite().getTextureRect());
 
 	if (car->getBulletSize() != 0) {
 		for (int i = 0; i < car->getBulletSize(); i++) {
@@ -109,6 +125,12 @@ void Game::draw()
 
 	window->draw(car->getText());
 	window->draw(car->getMagazine());
+
+	if (theEnd == true)
+	{
+		window->draw(sEndScreen);
+		window->draw(sChooseBar);
+	}
 	window->display();
 }
 
@@ -128,6 +150,7 @@ void Game::pointsGather()
 		{ 
 			car->scoreUp();
 			traffic.erase(traffic.begin() + i);
+			car->lvlUp();
 		}
 	}
 }
@@ -141,9 +164,32 @@ void Game::updateEvents(sf::RenderWindow* window, sf::Event& event)
 		{
 			if (event.key.code == sf::Keyboard::Escape)
 			{
-				window->close();
+				theEnd = true;
 			}
-		}		
+		}	
+		if (theEnd == true)
+		{
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::Enter)
+				{
+					if (sChooseBar.getPosition().x == 178)
+					{
+						theEnd = false;
+						setup();
+					}
+					else if (sChooseBar.getPosition().x == 284) window->close();
+				}
+				if (event.key.code == sf::Keyboard::Right)
+				{
+					sChooseBar.setPosition(284, 462);
+				}
+				else if (event.key.code == sf::Keyboard::Left)
+				{
+					sChooseBar.setPosition(178, 462);
+				}
+			}
+		}
 	}
 }
 /*
